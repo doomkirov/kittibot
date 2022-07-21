c03533e7ea884c974fdad1438b7d0f0993536085
doomkirov <104706005+doomkirov@users.noreply.github.com>
2022-07-21 18:40:36 +0300

    due to token

--- send_random_image.py
+++
@@ -1,9 +0,0 @@
-import requests
-from telegram import Bot
-
-bot = Bot(token='5534444700:AAGqlDYK820tpT4tVaFBe99O1vV8sUly2pY')
-URL = 'https://api.thecatapi.com/v1/images/search'
-response = requests.get(URL).json()
-chat_id = 5245286301
-random_cat_url = response[0].get('url')
-bot.send_photo(chat_id, random_cat_url)
