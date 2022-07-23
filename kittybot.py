b720fcd13b060296342d379b27d90d0e7b67c528
doomkirov <104706005+doomkirov@users.noreply.github.com>
2022-07-23 06:22:28 +0300

    __name__ == main

--- kittybot.py
+++ kittybot.py
@@ -39,8 +39,12 @@ def wake_up(update, context):
     )
     context.bot.send_photo(chat.id, get_new_image())
 
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
-updater.start_polling()
-updater.idle()
+def main():
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
